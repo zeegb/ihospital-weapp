f26455fc3177e3c46bc3a98afe4c15061fbf75bc
wangze <neu_wangze@126.com>
2017-01-10 11:41:18 +0800

    init && home page

---
+++ pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    title: '网络医院',
+    userInfo: {}
+  },
+  onReady() {
+    const self = this;
+    wx.setNavigationBarTitle({
+      title: self.data.title,
+    });
+  },
+  //事件处理函数
+  viewNext(e) {
+    const ds = e.currentTarget.dataset;
+    console.log(ds)
+    wx.navigateTo({
+      url: `../${ds.destination}/${ds.destination}`
+    });
+  },
+  onLoad() {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
